b05b491ccd67df97c8c03352a20edec0bc1de9eb
luca <luca.digrazia94@gmail.com>
2020-04-29 17:54:20 +0200

    Extraction implemented with a small test.

--- Code/main.py
+++ Code/main.py
@@ -20,26 +20,19 @@ def extract_from_file(file_path: str):
     param_types = type_collector.param_annotations
     return_types = type_collector.return_types
 
-    #print(f"params: {param_types}, returns: {return_types}")
-    #return f"params: {param_types}, returns: {return_types}"
-    return param_types,return_types
+    # print(f"params: {param_types}, returns: {return_types}")
+    # return f"params: {param_types}, returns: {return_types}"
+    return param_types, return_types
+
 
 def writeJSON(filename, change_list):
-    """
-    try:
-        with open(config.ROOT_DIR + "/Resources/Output/" + filename + ".json") as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        data = {}
-    if not data:
-        data = {}
-    """
-    # data = [object]
+    json_file = json.dumps([change.__dict__ for change in change_list], indent=4)
 
-    s = json.dumps([change.__dict__ for change in change_list])
+    print(json_file)
 
     with open(config.ROOT_DIR + "/Resources/Output/" + filename + ".json", "w") as f:
-        json.dump(s, f, indent=4)
+        f.write(json_file)
+    f.close()
 
 
 def search_multiple_strings_in_file(file_name, list_of_strings):
@@ -53,17 +46,40 @@ def search_multiple_strings_in_file(file_name, list_of_strings):
             line_number += 1
             # For each line, check if line contains any string from the list of strings
             for string_to_search in list_of_strings:
-                if string_to_search in line:
+                if ''.join(string_to_search) in line:
                     # If any string is found in line, then append that line along with line number in list
                     list_of_results.append((string_to_search, line_number, line.rstrip()))
 
     # Return list of tuples containing matched string, line numbers and lines where string is found
     return list_of_results
 
-if __name__ == "__main__":
-    old_param_types, old_return_types  = extract_from_file('/home/luca/PycharmProjects/TypeAnnotation_Study/Resources/Input/old.py')
 
-    new_param_types, new_return_types = extract_from_file('/home/luca/PycharmProjects/TypeAnnotation_Study/Resources/Input/new.py')
+def search_key_value_in_file(file_name, list_of_strings):
+    """Get line from the file along with line numbers, which contains any string from the list"""
+    line_number = 0
+    # Open the file in read only mode
+    with open(file_name, 'r') as read_obj:
+        # Read all lines in the file one by one
+        for line in read_obj:
+            line_number += 1
+            # TODO: Add regex for edge cases
+            str = "(".join(list_of_strings[0])
+            str2 = "".join(list_of_strings[1])
+            l = line
+            if "(".join(list_of_strings[0]) in line and "".join(list_of_strings[1]) in line.replace(" ", ""):
+                # If any string is found in line, then append that line along with line number in list
+                return line_number, l.rstrip()
+
+    # Return list of tuples containing matched string, line numbers and lines where string is found
+    return False
+
+
+if __name__ == "__main__":
+    code_changes = []
+
+    old_param_types, old_return_types = extract_from_file(config.ROOT_DIR + '/Resources/Input/old.py')
+
+    new_param_types, new_return_types = extract_from_file(config.ROOT_DIR + '/Resources/Input/new.py')
 
     print(old_param_types)
     print(new_param_types)
@@ -73,16 +89,26 @@ if __name__ == "__main__":
     print(old_return_types)
     print(new_return_types)
 
+    print("\n")
+
     for key in old_return_types:
-        pass
+        if old_return_types[key] != new_return_types[key]:
+            old_line, old_code = search_key_value_in_file(config.ROOT_DIR + '/Resources/Input/old.py',
+                                                          [key, old_return_types[key]])
+            new_line, new_code = search_key_value_in_file(config.ROOT_DIR + '/Resources/Input/new.py',
+                                                          [key, new_return_types[key]])
 
-   # diff=difflib.unified_diff(old, new)
+            code_changes.append(CodeChange("32321kj321k", old_line, old_code, new_line, new_code))
 
-   # print("".join(diff))
+    for key in old_param_types:
+        s = old_param_types[key]
+        ss = new_param_types[key]
+        if old_param_types[key] != new_param_types[key]:
+            old_line, old_code = search_key_value_in_file(config.ROOT_DIR + '/Resources/Input/old.py',
+                                                          [key, old_param_types[key]])
+            new_line, new_code = search_key_value_in_file(config.ROOT_DIR + '/Resources/Input/new.py',
+                                                          [key, new_param_types[key]])
 
+            code_changes.append(CodeChange("32321kj321k", old_line, old_code, new_line, new_code))
 
-    change_list = []
-    change_list.append(CodeChange("3jdi32d3", "234", "if(x>)", "while(x<0)"))
-    change_list.append(CodeChange("3jdi32d3", "234", "if(x>)", "while(x<0)"))
- #   writeJSON("typeAnnotationChanges", change_list)
-
+    writeJSON("typeAnnotationChanges", code_changes)
