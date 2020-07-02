c0284f1773f2dfd9a3decef0362a156662715073
luca <luca.digrazia94@gmail.com>
2020-07-02 10:38:03 +0200

    Managing error git clone

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 TEST = False
-CLONING = True
+CLONING = False
 
 REPO_LIST = ROOT_DIR + '/Resources/Input/repositoriesTOP1000.json'
