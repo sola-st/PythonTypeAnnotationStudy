e16cd6c745706febdb408c06cf537f79a1f755b2
Michael Pradel <michael@binaervarianz.de>
2020-09-28 11:37:25 +0200

    adding code to analyze type errors (moved from my personal repo to here, so that everything is in one place)

---
+++ Code/AnalyzeRepos.py
@@ -0,0 +1,272 @@
+#!/usr/bin/python3
+
+import subprocess
+import sys
+import re
+from collections import Counter
+import json
+from os import path, scandir
+from TypeAnnotationCounter import count_type_annotations
+
+repos_base_dir = "./data/repos/"
+results_base_dir = "./data/results/"
+
+
+def find_all_projects():
+    projects = []
+    for e in scandir(repos_base_dir):
+        if e.is_dir():
+            projects.append(e.name)
+    return projects
+
+
+projects = find_all_projects()
+
+
+def init_pyre(repo_dir):
+    if not path.isfile(repo_dir+"/.pyre_configuration"):
+        print(f"Creating pyre configuration for {repo_dir}")
+        subprocess.run(
+            f"cp data/.pyre_configuration {repo_dir}".split(" "))
+    else:
+        print(f"Reusing existing pyre configuration for {repo_dir}")
+
+
+def invoke_cmd(cmd, cwd):
+    try:
+        out = subprocess.check_output(
+            cmd.split(" "), cwd=cwd)
+        out = out.decode(sys.stdout.encoding)
+    except subprocess.CalledProcessError as e:
+        # handle exit codes, e.g., when type checker reports warnings
+        out = e.output.decode(sys.stdout.encoding)
+    return out
+
+
+def check_commit(repo_dir, commit):
+    print("\n===========================================")
+    print(f"Checking commit {commit} of {repo_dir}")
+
+    # go to commit
+    subprocess.run(f"git checkout {commit}".split(" "), cwd=repo_dir)
+
+    # get date of commit
+    out = subprocess.check_output(
+        f"git show -s --format=%ci {commit}".split(" "), cwd=repo_dir)
+    commit_date = out.decode(sys.stdout.encoding).rstrip()
+
+    # count lines of code
+    print("--- Counting lines of code")
+    out = invoke_cmd(f"sloccount .", repo_dir)
+
+    loc = 0
+    for l in out.split("\n"):
+        l_search = re.search(r"python:\s*(\d+) .*", l)
+        if l_search is not None:
+            loc = int(l_search.group(1))
+
+    # count Python files
+    out = invoke_cmd(f"find . -name *.py", repo_dir)
+    nb_python_files = len(out.split("\n")) - 1  # last line is empty
+
+    # type check
+    print("--- Type checking")
+    out = invoke_cmd("pyre check", repo_dir)
+    warnings = out.split("\n")
+    warnings = warnings[:-1]  # last line is empty
+    print(f"Got {len(warnings)} warnings")
+
+    # analyze warnings
+    kind_to_nb = Counter()
+    for w in warnings:
+        w_search = re.search(r".*:\d+:\d+ (.*\[\d+\]):.*", w)
+        if w_search is None:
+            raise Exception(f"Warning: Could not parse warning -- {w}")
+        warning_kind = w_search.group(1)
+        kind_to_nb[warning_kind] += 1
+
+    # count type annotations
+    param_types, return_types, variable_types = count_type_annotations(
+        repo_dir)
+
+    result = {
+        "commit": commit,
+        "commit_date": commit_date,
+        "loc": loc,
+        "nb_python_files": nb_python_files,
+        "nb_param_types": param_types,
+        "nb_return_types": return_types,
+        "nb_variable_types": variable_types,
+        "nb_warnings": len(warnings),
+        "kind_to_nb": kind_to_nb
+    }
+    return result
+
+
+def nb_types(r):
+    return r["nb_param_types"] + r["nb_variable_types"] + r["nb_return_types"]
+
+
+def get_all_commits(repo_dir):
+    out = invoke_cmd("git log --all --oneline", repo_dir)
+    lines = out.split("\n")
+    lines = lines[:-1]  # last line is empty
+    commits = [l.split(" ")[0] for l in lines]
+    print(f"Found {len(commits)} commits")
+    return commits
+
+
+def write_results(name, results):
+    with open(results_base_dir+name+".json", "w") as fp:
+        fp.write(json.dumps(results, indent=2))
+
+
+def sample_commits(all_commits, max_commits_per_project):
+    if len(all_commits) <= max_commits_per_project:
+        commits = all_commits
+    else:
+        stride = int(round(len(all_commits) /
+                           float(max_commits_per_project - 1)))
+        commits = [all_commits[i]
+                   for i in range(0, len(all_commits), stride)]
+    return commits
+
+
+def analyze_histories(projects, max_commits_per_project):
+    for p in projects:
+        try:
+            repo_dir = repos_base_dir+p
+            init_pyre(repo_dir)
+            all_commits = get_all_commits(repo_dir)
+            commits = sample_commits(all_commits, max_commits_per_project)
+            project_results = []
+            for c in commits:
+                r = check_commit(repo_dir, c)
+                project_results.append(r)
+            write_results("history_"+p, project_results)
+        except Exception as e:
+            print(f"WARNING: Some problem with {p} -- skipping this project")
+            print(e)
+
+
+def analyze_latest_commit(projects):
+    results = []
+    for p in projects:
+        repo_dir = repos_base_dir+p
+        init_pyre(repo_dir)
+        all_commits = get_all_commits(repo_dir)
+        latest_commit = all_commits[0]
+        r = check_commit(repo_dir, latest_commit)
+        r["project"] = p
+        results.append(r)
+    write_results("latest", results)
+
+
+def get_parent_commit(repo_dir, commit):
+    cmd = f"git log --pretty=%P -n 1 {commit}"
+    out = invoke_cmd(cmd, repo_dir)
+    return out.split("\n")[0]
+
+
+def is_add_only_commit(c):
+    return (c["added_per_commit_percentage"] == "100.0 %" and
+            int(c["typeannotation_line_inserted"]) > 0 and
+            int(c["typeannotation_line_removed"]) == 0 and
+            int(c["typeannotation_line_changed"]) == 0)
+
+
+def is_remove_only_commit(c):
+    return (c["removed_per_commit_percentage"] == "100.0 %" and
+            int(c["typeannotation_line_inserted"]) == 0 and
+            int(c["typeannotation_line_removed"]) > 0 and
+            int(c["typeannotation_line_changed"]) == 0)
+
+
+def is_changed_only_commit(c):
+    return (c["changed_per_commit_percentage"] == "100.0 %" and
+            int(c["typeannotation_line_inserted"]) == 0 and
+            int(c["typeannotation_line_removed"]) == 0 and
+            int(c["typeannotation_line_changed"]) > 0)
+
+
+def analyze_specific_commits(commits_file):
+    with open(commits_file) as fp:
+        commit_stats = json.load(fp)
+
+    result = {
+        "add_only_commits": {
+            "same_nb_errors": 0,
+            "more_errors": 0,
+            "fewer_errors": 0,
+        },
+        "remove_only_commits": {
+            "same_nb_errors": 0,
+            "more_errors": 0,
+            "fewer_errors": 0,
+        },
+        "change_only_commits": {
+            "same_nb_errors": 0,
+            "more_errors": 0,
+            "fewer_errors": 0,
+        },
+    }
+    commit_url_regexp = r"https.*github\.com\/(.*)\/(.*)\/commit\/(.*)"
+    skipped = 0
+    used = 0
+    for c in commit_stats:
+        add_only = is_add_only_commit(c)
+        remove_only = is_remove_only_commit(c)
+        change_only = is_changed_only_commit(c)
+
+        if add_only or remove_only or change_only:
+            commit_url = c["url"]
+            match = re.match(commit_url_regexp, commit_url)
+            project = match.group(2)
+            commit = match.group(3)
+            repo_dir = repos_base_dir+project
+            parent_commit = get_parent_commit(repo_dir, commit)
+
+            pre_change = check_commit(repo_dir, parent_commit)
+            post_change = check_commit(repo_dir, commit)
+
+            # TODO: if data from Luca is correct, this shouldn't be necessary
+            if add_only and not (nb_types(pre_change) < nb_types(post_change)):
+                skipped += 1
+                continue
+            if remove_only and not (nb_types(pre_change) > nb_types(post_change)):
+                skipped += 1
+                continue
+            if change_only and not (nb_types(pre_change) == nb_types(post_change)):
+                skipped += 1
+                continue
+
+            # write result for this commit into overall stats
+            if add_only:
+                relevant_result = result["add_only_commits"]
+            elif remove_only:
+                relevant_result = result["remove_only_commits"]
+            else:  # change-only
+                relevant_result = result["change_only_commits"]
+            warnings_diff = post_change["nb_warnings"] - \
+                pre_change["nb_warnings"]
+            if warnings_diff == 0:
+                relevant_result["same_nb_errors"] += 1
+            elif warnings_diff > 0:
+                relevant_result["more_errors"] += 1
+            elif warnings_diff < 0:
+                relevant_result["fewer_errors"] += 1
+
+            used += 1
+            if used % 10 == 0:
+                print(f"\nUsed {used}, skipped {skipped}")
+                print(f"{result}")
+
+    write_results("pure_commits_vs_errors.json", result)
+
+
+if __name__ == "__main__":
+    # analyze_histories(projects, max_commits_per_project=11)
+    # analyze_latest_commit(projects)  # TODO: still needed?
+
+    analyze_specific_commits(
+        "data/results_luca/typeAnnotationCommitStatistics.json")
---
+++ Code/PlotResultsAndComputeStats.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python3
+
+from collections import Counter
+import json
+import re
+import matplotlib.pyplot as plt
+from AnalyzeRepos import results_base_dir, projects
+import pandas as pd
+
+
+plots_base_dir = "./data/plots/"
+
+
+ignored_warning_kinds = [
+    "Undefined import [21]",
+    "Undefined or invalid type [11]",
+    "Undefined name [18]",
+    "Undefined attribute [16]",
+]
+
+
+def read_results(name):
+    with open(results_base_dir+name+".json") as fp:
+        r = json.load(fp)
+    return r
+
+
+def get_results():
+    project_to_history = {}
+    latest_results = []
+    for p in projects:
+        try:
+            p_results = read_results("history_"+p)
+        except Exception as e:
+            print(
+                f"WARNING: Couldn't read results for {p}. Skipping this project.")
+            continue
+        project_to_history[p] = p_results
+        latest_p_result = p_results[0]
+        latest_p_result["project"] = p
+        latest_results.append(latest_p_result)
+    print(f"Read {len(project_to_history)} project histories")
+    return project_to_history, pd.DataFrame(latest_results)
+
+
+def count_filtered_warnings(kind_to_nb):
+    total = 0
+    for kind, nb in kind_to_nb.items():
+        if kind not in ignored_warning_kinds:
+            total += nb
+    return total
+
+
+def compute_more_columns(results):
+    warnings = []
+    for kind_to_nb in results["kind_to_nb"]:
+        nb_warnings = count_filtered_warnings(kind_to_nb)
+        warnings.append(nb_warnings)
+    results["nb_filtered_warnings"] = warnings
+
+    results["nb_types"] = results["nb_param_types"] + \
+        results["nb_return_types"] + results["nb_variable_types"]
+
+
+def plot_warnings_loc_evolution(p):
+    results = read_results("history_"+p)
+    dates = []
+    locs = []
+    warnings = []
+    for r in reversed(results):
+        date = r["commit_date"]
+        date = date.split(" ")[0]
+        dates.append(date)
+        locs.append(r["loc"])
+        nb_warnings = count_filtered_warnings(r["kind_to_nb"])
+        warnings.append(nb_warnings)
+
+    print(dates)
+    print(warnings)
+    print(locs)
+
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel("Date")
+    plt.xticks(rotation=90)
+    ax1.set_ylabel("Type warnings")
+    ax1.plot(dates, warnings, label="Type warnings", marker="o")
+    ax1.legend()
+    ax1.set_ylim(bottom=0)
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Lines of code")
+    ax2.plot(dates, locs, label="Lines of code",
+             linestyle="dashed", marker="x")
+    ax2.legend()
+    ax2.set_ylim(bottom=0)
+    fig.tight_layout()
+    ax1.set_title(f"Evolution of {p}")
+    plt.savefig(plots_base_dir+"warnings_log_evolution_"+p+".pdf")
+    plt.close()
+
+
+def plot_kinds_of_errors(results):
+    total_kind_to_nb = Counter()
+    for kind_to_nb in results["kind_to_nb"]:
+        total_kind_to_nb.update(Counter(kind_to_nb))
+
+    kinds = []
+    nbs = []
+    kind_matcher = re.compile(r" \[.*")
+    for kind, nb in total_kind_to_nb.most_common():
+        if kind not in ignored_warning_kinds:
+            kind = kind_matcher.sub("", kind)
+            kinds.append(kind)
+            nbs.append(nb)
+
+    y_pos = range(len(kinds))
+    plt.bar(y_pos, nbs, align='center', alpha=0.5)
+    plt.xticks(y_pos, kinds)
+    plt.xticks(rotation=90)
+    plt.ylabel("Number of warnings")
+    plt.title("Prevalence of kinds of type warnings")
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"kinds_of_warnings.pdf")
+    plt.close()
+
+
+def plot_errors_vs_loc(results):
+    plt.plot(results["loc"], results["nb_filtered_warnings"], "o")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Lines of code")
+    plt.ylabel("Number of warnings")
+    plt.title("Type errors vs. code size")
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"errors_vs_loc.pdf")
+    plt.close()
+
+
+def plot_evolution_of_errors_vs_loc(project_to_history):
+    for p, h in project_to_history.items():
+        errors_per_loc = []
+        for r in h:
+            loc = r["loc"]
+            nb_warnings = count_filtered_warnings(r["kind_to_nb"])
+            if loc > 0:
+                errors_per_loc.append(nb_warnings / loc)
+            else:
+                errors_per_loc.append(0)
+        plt.plot(errors_per_loc)
+    plt.xlabel("Time steps during version history")
+    plt.ylabel("Type errors per lines of code")
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"errors_vs_loc_evolution.pdf")
+    plt.close()
+
+
+def plot_errors_vs_annotations(results):
+    plt.plot(results["nb_types"], results["nb_filtered_warnings"], "o")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Type annotations")
+    plt.ylabel("Number of warnings")
+    plt.title("Type errors vs. annotations")
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"errors_vs_annotations.pdf")
+    plt.close()
+
+
+def plot_evolution_of_errors_vs_annotations(project_to_history):
+    for p, h in project_to_history.items():
+        errors_per_annotations = []
+        for r in h:
+            nb_annotations = r["nb_param_types"] + \
+                r["nb_return_types"] + r["nb_variable_types"]
+            nb_warnings = count_filtered_warnings(r["kind_to_nb"])
+            if nb_annotations > 0:
+                errors_per_annotations.append(nb_warnings / nb_annotations)
+            else:
+                errors_per_annotations.append(0)
+        plt.plot(errors_per_annotations)
+
+    plt.xlabel("Time steps during version history")
+    plt.ylabel("Type errors per annotation")
+    plt.ylim(0, 50)
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"errors_vs_annotations_evolution.pdf")
+    plt.close()
+
+
+def plot_evolution_of_avg_errors_vs_annotations(project_to_history):
+    sum_at_step = [0.0] * 10
+    for p, h in project_to_history.items():
+        for step, r in enumerate(h[:10]):
+            nb_annotations = r["nb_param_types"] + \
+                r["nb_return_types"] + r["nb_variable_types"]
+            nb_warnings = count_filtered_warnings(r["kind_to_nb"])
+            if nb_annotations > 0:
+                sum_at_step[step] += nb_warnings / nb_annotations
+    avg_at_step = [s / len(project_to_history) for s in sum_at_step]
+    plt.plot(avg_at_step)
+    plt.xlabel("Time steps during version history")
+    plt.ylabel("Type errors per annotation (avg. across projects)")
+    plt.tight_layout()
+    plt.savefig(plots_base_dir+"errors_vs_annotations_evolution_avg.pdf")
+    plt.close()
+
+
+def plot_per_project_evolution(project_to_history):
+    plt.rcParams.update({'font.size': 16})
+    for p, h in project_to_history.items():
+        annotation_evol = []
+        loc_evol = []
+        for r in reversed(h[:10]):
+            annotation_evol.append(r["nb_param_types"] +
+                                   r["nb_return_types"] + r["nb_variable_types"])
+            loc_evol.append(r["loc"])
+        _, ax1 = plt.subplots()
+        plt.xlabel(f"Time steps during version history of {p}")
+        ax1.set_ylim(bottom=0, top=max(loc_evol)*1.05)
+        line1 = ax1.plot(loc_evol, label="Lines of code (left)",
+                         marker="o", color="blue")
+        ax2 = ax1.twinx()
+        ax2.set_ylim(bottom=0, top=max(max(annotation_evol)*1.05, 20))
+        line2 = ax2.plot(
+            annotation_evol, label="Type annotations (right)", marker="x", color="brown")
+        lines = line1 + line2
+        labels = [l.get_label() for l in lines]
+        plt.legend(lines, labels)
+        plt.tight_layout()
+        plt.savefig(f"{plots_base_dir}per_project/{p}.pdf")
+        plt.close()
+
+
+def compute_stats_on_histories(project_to_history):
+    print("\n------------------")
+    print("Statistics on histories:")
+    commits_total = 0
+    commits_with_errors = 0
+    for p, h in project_to_history.items():
+        for r in h:
+            commits_total += 1
+            nb_warnings = count_filtered_warnings(r["kind_to_nb"])
+            if nb_warnings > 0:
+                commits_with_errors += 1
+    print(f"  {commits_total} total commits")
+    print(f"  {commits_with_errors} commits with errors ({round(commits_with_errors*100 / commits_total, 1)}%)")
+
+
+def compute_stats_on_latest(results):
+    print("\n------------------")
+    print("Statistics on latest versions:")
+    loc_total = results["loc"].sum()
+    errors_total = results["nb_filtered_warnings"].sum()
+
+    print(f"  {loc_total} total LoC in latest versions")
+    print(f"  {errors_total} total errors in latest versions")
+    print(f"  {round(errors_total / loc_total, 3)} errors per LoC, on average")
+
+    corr_loc_errors = results["loc"].corr(results["nb_filtered_warnings"])
+    print(f"  Correlation between errors and LoC: {corr_loc_errors}")
+    corr_annotations_errors = results["nb_types"].corr(
+        results["nb_filtered_warnings"])
+    print(
+        f"  Correlation between errors and annotations: {corr_annotations_errors}")
+
+
+if __name__ == "__main__":
+    project_to_history, latest_results = get_results()
+    compute_more_columns(latest_results)
+
+    plot_kinds_of_errors(latest_results)
+    plot_errors_vs_loc(latest_results)
+    plot_errors_vs_annotations(latest_results)
+
+    plot_per_project_evolution(project_to_history)
+    plot_evolution_of_errors_vs_annotations(project_to_history)
+    plot_evolution_of_avg_errors_vs_annotations(project_to_history)
+    plot_evolution_of_errors_vs_loc(project_to_history)
+
+    compute_stats_on_latest(latest_results)
+    compute_stats_on_histories(project_to_history)
+
+    #    plot_warnings_loc_evolution(p)
