7299636817b527dcc2da70e7eeba9581bbdb0066
luca <luca.digrazia94@gmail.com>
2020-11-30 11:04:51 +0100

    Minor fix.

--- PlotResultsAndComputeStats.py
+++ PlotResultsAndComputeStats.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import config
 from script_AnalyzeRepos import results_base_dir, projects
 import pandas as pd
+font_size = 15
 
 
 plots_base_dir = config.ROOT_DIR + "/Resources/Output_typeErrors/"
@@ -80,17 +81,19 @@ def plot_warnings_loc_evolution(p):
     print(warnings)
     print(locs)
 
+    plt.rcParams.update({'font.size': font_size})
+
     fig, ax1 = plt.subplots()
     ax1.set_xlabel("Date")
     plt.xticks(rotation=90)
     ax1.set_ylabel("Type warnings")
-    ax1.plot(dates, warnings, label="Type warnings", marker="o")
+    ax1.plot(dates, warnings, label="Type warnings", marker="o", color=(0.2, 0.4, 0.6, 0.6))
     ax1.legend()
     ax1.set_ylim(bottom=0)
     ax2 = ax1.twinx()
     ax2.set_ylabel("Lines of code")
     ax2.plot(dates, locs, label="Lines of code",
-             linestyle="dashed", marker="x")
+             linestyle="dashed", marker="x", color='lightsalmon')
     ax2.legend()
     ax2.set_ylim(bottom=0)
     fig.tight_layout()
@@ -113,8 +116,9 @@ def plot_kinds_of_errors(results):
             kinds.append(kind)
             nbs.append(nb)
 
+    plt.rcParams.update({'font.size': font_size})
     y_pos = range(len(kinds))
-    plt.bar(y_pos, nbs, align='center', alpha=0.5)
+    plt.bar(y_pos, nbs, align='center', alpha=0.5, color=(0.2, 0.4, 0.6, 0.6))
     plt.xticks(y_pos, kinds)
     plt.xticks(rotation=90)
     plt.ylabel("Number of warnings")
@@ -125,7 +129,8 @@ def plot_kinds_of_errors(results):
 
 
 def plot_errors_vs_loc(results):
-    plt.plot(results["loc"], results["nb_filtered_warnings"], "o")
+    plt.rcParams.update({'font.size': font_size})
+    plt.plot(results["loc"], results["nb_filtered_warnings"], "o", color=(0.2, 0.4, 0.6, 0.6))
     plt.xscale("log")
     plt.yscale("log")
     plt.xlabel("Lines of code")
@@ -137,6 +142,7 @@ def plot_errors_vs_loc(results):
 
 
 def plot_evolution_of_errors_vs_loc(project_to_history):
+    plt.rcParams.update({'font.size': font_size})
     for p, h in project_to_history.items():
         errors_per_loc = []
         for r in h:
@@ -146,7 +152,7 @@ def plot_evolution_of_errors_vs_loc(project_to_history):
                 errors_per_loc.append(nb_warnings / loc)
             else:
                 errors_per_loc.append(0)
-        plt.plot(errors_per_loc)
+        plt.plot(errors_per_loc, color=(0.2, 0.4, 0.6, 0.6))
     plt.xlabel("Time steps during version history")
     plt.ylabel("Type errors per lines of code")
     plt.tight_layout()
@@ -155,7 +161,8 @@ def plot_evolution_of_errors_vs_loc(project_to_history):
 
 
 def plot_errors_vs_annotations(results):
-    plt.plot(results["nb_types"], results["nb_filtered_warnings"], "o")
+    plt.rcParams.update({'font.size': font_size})
+    plt.plot(results["nb_types"], results["nb_filtered_warnings"], "o", color=(0.2, 0.4, 0.6, 0.6))
     plt.xscale("log")
     plt.yscale("log")
     plt.xlabel("Type annotations")
@@ -167,6 +174,7 @@ def plot_errors_vs_annotations(results):
 
 
 def plot_evolution_of_errors_vs_annotations(project_to_history):
+    plt.rcParams.update({'font.size': font_size})
     for p, h in project_to_history.items():
         errors_per_annotations = []
         for r in h:
@@ -177,7 +185,7 @@ def plot_evolution_of_errors_vs_annotations(project_to_history):
                 errors_per_annotations.append(nb_warnings / nb_annotations)
             else:
                 errors_per_annotations.append(0)
-        plt.plot(errors_per_annotations)
+        plt.plot(errors_per_annotations, color=(0.2, 0.4, 0.6, 0.6))
 
     plt.xlabel("Time steps during version history")
     plt.ylabel("Type errors per annotation")
@@ -188,6 +196,7 @@ def plot_evolution_of_errors_vs_annotations(project_to_history):
 
 
 def plot_evolution_of_avg_errors_vs_annotations(project_to_history):
+    plt.rcParams.update({'font.size': font_size})
     sum_at_step = [0.0] * 10
     for p, h in project_to_history.items():
         for step, r in enumerate(h[:10]):
@@ -197,15 +206,16 @@ def plot_evolution_of_avg_errors_vs_annotations(project_to_history):
             if nb_annotations > 0:
                 sum_at_step[step] += nb_warnings / nb_annotations
     avg_at_step = [s / len(project_to_history) for s in sum_at_step]
-    plt.plot(avg_at_step)
+    plt.plot(avg_at_step, color=(0.2, 0.4, 0.6, 0.6))
     plt.xlabel("Time steps during version history")
-    plt.ylabel("Type errors per annotation (avg. across projects)")
+    plt.ylabel("Type errors per annotation\n(avg. across projects)")
     plt.tight_layout()
     plt.savefig(plots_base_dir+"errors_vs_annotations_evolution_avg.pdf")
     plt.close()
 
 
 def plot_per_project_evolution(project_to_history):
+    plt.rcParams.update({'font.size': font_size})
     plt.rcParams.update({'font.size': 16})
     for p, h in project_to_history.items():
         annotation_evol = []
@@ -215,14 +225,14 @@ def plot_per_project_evolution(project_to_history):
                                    r["nb_return_types"] + r["nb_variable_types"])
             loc_evol.append(r["loc"])
         _, ax1 = plt.subplots()
-        plt.xlabel(f"Time steps during version history of {p}")
+        plt.xlabel(f"Time steps during version\nhistory of {p}")
         ax1.set_ylim(bottom=0, top=max(loc_evol)*1.05)
         line1 = ax1.plot(loc_evol, label="Lines of code (left)",
-                         marker="o", color="blue")
+                         marker="o", color=(0.2, 0.4, 0.6, 0.6))
         ax2 = ax1.twinx()
         ax2.set_ylim(bottom=0, top=max(max(annotation_evol)*1.05, 20))
         line2 = ax2.plot(
-            annotation_evol, label="Type annotations (right)", marker="x", color="brown")
+            annotation_evol, label="Type annotations (right)", marker="x", color='lightsalmon')
         lines = line1 + line2
         labels = [l.get_label() for l in lines]
         plt.legend(lines, labels)
