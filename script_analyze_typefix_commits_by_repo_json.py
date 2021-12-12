962a7613c8845d16463304777faea1b8f084a673
victorcwai@gmail.com <victorcwai@gmail.com>
2021-12-12 15:04:29 +0100

    Add watchmanconfig and handle timeout error

--- script_analyze_typefix_commits_by_repo_json.py
+++ script_analyze_typefix_commits_by_repo_json.py
@@ -12,13 +12,24 @@ import random
 import time
 from collections import Counter, defaultdict
 import json
-from os import path, scandir
 from Code.TypeErrors.TypeAnnotationCounter import count_type_annotations
 from Code.TypeAnnotations import gitUtils
 import config
+import signal
 
 repos_base_dir = config.ROOT_DIR + "/GitHub/"
 results_base_dir = config.ROOT_DIR + "/Resources/Output_type_fix_commits_via_API_repo_json/"
+
+class PyreTimeoutError(Exception):
+    """
+    Attributes:
+        pid -- pyre.bin process id
+    """
+
+    def __init__(self, pid, message="Pyre timeout"):
+        self.pid = pid
+        self.message = message+str(pid)
+        super().__init__(self.message)
 
 def memory_limit():
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
@@ -47,12 +58,17 @@ def init_pyre(repo_dir):
 
 def invoke_cmd(cmd, cwd):
     try:
-        out = subprocess.check_output(
-            cmd.split(" "), cwd=cwd, timeout=180) # timeout in 3 mins
+        # The os.setsid() is passed in the argument preexec_fn so
+        # it's run after the fork() and before  exec() to run the shell.
+        proc = subprocess.Popen(cmd.split(" "), cwd=cwd, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+        pid = proc.pid
+        out = proc.communicate(timeout=180)[0]
         out = out.decode(sys.stdout.encoding)
-    except subprocess.CalledProcessError as e:
-        # handle exit codes, e.g., when type checker reports warnings
-        out = e.output.decode(sys.stdout.encoding)
+    except subprocess.TimeoutExpired as e:
+        raise PyreTimeoutError(pid)
+    # except subprocess.CalledProcessError as e:
+    #     # handle exit codes, e.g., when type checker reports warnings
+    #     out = e.output.decode(sys.stdout.encoding)
     return out
 
 def get_commit_type_error(repo_dir, commit): # repo_dir = /home/wai/hiwi/TypeAnnotation_Study/GitHub/Python
@@ -88,8 +104,6 @@ def get_commit_type_error(repo_dir, commit): # repo_dir = /home/wai/hiwi/TypeAnn
 
     # type check
     print("--- Type checking")
-    if not path.isfile(repo_dir+"/.pyre_configuration"):
-        out = invoke_cmd("cp ../.pyre_configuration .", repo_dir)
     out = invoke_cmd("pyre incremental", repo_dir)
     
     warnings = out.split("\n")
@@ -156,51 +170,92 @@ def sample_commits(all_commits, max_commits_per_project):
 def compare_two_commits_warnings_output(p_commit_pair):   
     p, commits = p_commit_pair
     repo_dir = repos_base_dir+p
+    pyre_server_up = False
+    if not os.path.isfile(repo_dir+"/.pyre_configuration"):
+        print(f"Creating pyre configuration for {repo_dir}")
+        subprocess.run(
+            f"cp data/.pyre_configuration {repo_dir}".split(" "))
+    if not os.path.isfile(repo_dir+"/.watchmanconfig"):
+        print(f"Creating watchman configuration for {repo_dir}")
+        subprocess.run(
+            f"cp data/.watchmanconfig {repo_dir}".split(" "))
     for b_commit in commits:
         a_commit = b_commit+'^'
         if os.path.isfile(results_base_dir+"compare_warning_"+p+"_"+a_commit+"_"+b_commit+".json"):
             print(f"Skip commit, data {p}_{a_commit}_{b_commit} already processed.")
             continue
-
-        try:
-            init_pyre(repo_dir)            
-            project_results = []
-            a_res = get_commit_type_error(repo_dir, a_commit)
-            b_res = get_commit_type_error(repo_dir, b_commit)
-            out = {
-                'project': p,
+        else:
+            print(invoke_cmd("watchman watch .", repo_dir)) # must have watchman watching for pyre incremental to work
+            pyre_server_up = True
+            try:
+                project_results = []
+                a_res = get_commit_type_error(repo_dir, a_commit)
+                b_res = get_commit_type_error(repo_dir, b_commit)
+                out = {
+                    'project': p,
+                    'a_commit': a_commit, 
                 'a_commit': a_commit, 
+                    'a_commit': a_commit, 
+                    'b_commit': b_commit, 
                 'b_commit': b_commit, 
-                'warning_removed': a_res['nb_warnings'] - b_res['nb_warnings'],
-                'parent_warnings': [],
-                'warnings': []
-            }
-            parent_files = a_res['file_to_count']
-            files = b_res['file_to_count']
-            for f, count in parent_files.items():
-                if f in files and files[f] < count:
-                    out['parent_warnings'].append([i for i in a_res['all_warnings'] if i.split(':')[0] in f])
-                    out['warnings'].append([i for i in b_res['all_warnings'] if i.split(':')[0] in f])
-                    # Remove warnings if the same msg exist in both parent_warnings and warnings
-                    for pw in list(out['parent_warnings'][-1]):
-                        pw_msg = pw.split(':')[-1]
-                        for w in list(out['warnings'][-1]):
-                            if pw_msg in w:
-                                if pw in out['parent_warnings'][-1]:
-                                    out['parent_warnings'][-1].remove(pw)
-                                if w in out['warnings'][-1]:
-                                    out['warnings'][-1].remove(w)
-                                break
-                elif f not in files:
-                    out['parent_warnings'].append([i for i in a_res['all_warnings'] if i.split(':')[0] in f])
-            project_results.append(out)
-            write_results("compare_warning_"+p+"_"+a_commit+"_"+b_commit, project_results)
-        except Exception as e:
-            print(f"WARNING: Some problem with {p} -- skipping this project")
-            write_results("compare_warning_"+p+"_"+a_commit+"_"+b_commit, []) # write empty files so that it won't be processed again
-            print(e)
+                    'b_commit': b_commit, 
+                    'warning_removed': a_res['nb_warnings'] - b_res['nb_warnings'],
+                    'parent_warnings': [],
+                    'warnings': []
+                }
+                parent_files = a_res['file_to_count']
+                files = b_res['file_to_count']
+                for f, count in parent_files.items():
+                    if f in files and files[f] < count:
+                        out['parent_warnings'].append([i for i in a_res['all_warnings'] if i.split(':')[0] in f])
+                        out['warnings'].append([i for i in b_res['all_warnings'] if i.split(':')[0] in f])
+                        # Remove warnings if the same msg exist in both parent_warnings and warnings
+                        for pw in list(out['parent_warnings'][-1]):
+                            pw_msg = pw.split(':')[-1]
+                            for w in list(out['warnings'][-1]):
+                                if pw_msg in w:
+                                    if pw in out['parent_warnings'][-1]:
+                                        out['parent_warnings'][-1].remove(pw)
+                                    if w in out['warnings'][-1]:
+                                        out['warnings'][-1].remove(w)
+                                    break
+                    elif f not in files:
+                        out['parent_warnings'].append([i for i in a_res['all_warnings'] if i.split(':')[0] in f])
+                project_results.append(out)
+                write_results("compare_warning_"+p+"_"+a_commit+"_"+b_commit, project_results)
+            except PyreTimeoutError as pe:
+                # invoke_cmd("pyre kill", repo_dir) # Kill *all* pyre servers!!
+                os.killpg(os.getpgid(pe.pid), signal.SIGTERM)  # Send the signal to all the process groups
+                # print(invoke_cmd("pkill -c pyre.bin", repo_dir)) # Kill process
+                print(pe.pid)
+                print(f"WARNING: PyreTimeoutError with {p}: {b_commit} -- skipping this project")
+                fail_log = open("failed_repo.txt", "a")  # append mode
+                fail_log.write('Timeout: '+p+': '+b_commit+'\n')
+                fail_log.close()
+                print(invoke_cmd("watchman watch-del .", repo_dir)) # teardown watchman to reduce inotify instances (cannot watch if its too big)
+                break # Skip this project
+            except Exception as e:
+                print(f"WARNING: Some problem with {p} -- skipping this commit")
+                # write_results("compare_warning_"+p+"_"+a_commit+"_"+b_commit, []) # write empty files so that it won't be processed again
+                fail_log = open("failed_repo.txt", "a")  # append mode
+                fail_log.write(str(e) + ' : '+p+': '+b_commit+'\n')
+                fail_log.close()
+                print(e)
     # Shutdown the pyre server (each repo uses one) at the end
-    invoke_cmd("pyre stop", repo_dir)
+    if pyre_server_up:
+        try:
+            invoke_cmd("pyre stop", repo_dir)
+            print(invoke_cmd("watchman watch-del .", repo_dir)) # teardown watchman to reduce inotify instances (cannot watch if its too big)
+        except PyreTimeoutError as pe:
+            # invoke_cmd("pyre kill", repo_dir) # Kill *all* pyre servers!!
+            os.killpg(os.getpgid(pe.pid), signal.SIGTERM)  # Send the signal to all the process groups
+            # print(invoke_cmd("pkill -c pyre.bin", repo_dir)) # Kill process
+            print(pe.pid)
+            print('WARNING: Timeout when stopping: '+p+': '+b_commit+'\n')
+            fail_log = open("failed_repo.txt", "a")  # append mode
+            fail_log.write('Timeout when stopping: '+p+': '+b_commit+'\n')
+            fail_log.close()
+
 
 
 def get_parent_commit(repo_dir, commit):
